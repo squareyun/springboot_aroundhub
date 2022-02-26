f5a406581abe129118f9ec8b843cad51e4c7fd62
squareyun <squareyun@gmail.com>
2022-02-26 20:57:30 +0900

    PUT, DELETE API

---
+++ src/main/java/studio/square/testproject/controller/PutController.java
@@ -0,0 +1,49 @@
+package studio.square.testproject.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import studio.square.testproject.dto.MemberDTO;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    // http://localhost:8080/api/v1/put-api/defalut
+    @PutMapping(value = "/defalut")
+    public String putMethod() {
+        return "Hello World!; ";
+    }
+
+    // http://localhost:8080/api/v1/put-api/member
+    @PutMapping(value = "/member")
+    public String putMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // http://localhost:8080/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String putMemberDto1(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+
+    // http://localhost:8080/api/v1/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDTO putMemberDto2(@RequestBody MemberDTO memberDTO) {
+        return memberDTO; // json의 형태로 반환된다. 이 방식이 더 선호됨
+    }
+
+    // http://localhost:8080/api/v1/put-api/member3
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDTO> putMemberDto3(@RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDTO); // 202 코드로 응답
+    }
+}
