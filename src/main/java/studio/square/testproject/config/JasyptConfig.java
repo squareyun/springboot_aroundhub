2a56664215fdbe65bbaa218b37ada9100df15330
squareyun <squareyun@gmail.com>
2022-03-01 22:56:54 +0900

    프로퍼티 암호화

---
+++ src/main/java/studio/square/testproject/config/JasyptConfig.java
@@ -0,0 +1,34 @@
+package studio.square.testproject.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Github : https://github.com/ulisesbochio/jasypt-spring-boot
+ */
+@Configuration
+public class JasyptConfig {
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        // 실행 VM 옵션에 패스워드 넣어줘야함
+        // -Djasypt.encryptor.password="패스워드"
+        config.setPassword(System.getProperty("jasypt.encryptor.password"));
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256"); // 암호화 알고리즘
+        config.setKeyObtentionIterations(1000); // 반복할 해싱 횟수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64"); // 인코딩 방식
+
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
