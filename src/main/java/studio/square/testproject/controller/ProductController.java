eaf0b14623a900734b54465c1dd1762e067ca26f
squareyun <squareyun@gmail.com>
2022-05-09 23:00:03 +0900

    유효성 검증 (Validation)

--- src/main/java/studio/square/testproject/controller/ProductController.java
+++ src/main/java/studio/square/testproject/controller/ProductController.java
@@ -3,9 +3,13 @@ package studio.square.testproject.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import studio.square.testproject.data.dto.ProductDto;
 import studio.square.testproject.data.service.ProductService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
@@ -21,11 +25,10 @@ public class ProductController {
 
     @GetMapping(value="/product/{productId}")
     public ProductDto getProduct(@PathVariable String productId){
-
         long startTime =System.currentTimeMillis();
         LOGGER.info("[ProductController] perform {} of Around Hub API.", "getProduct");
 
-        ProductDto productDto =productService.getProduct(productId);
+        ProductDto productDto = productService.getProduct(productId);
 
         LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms", productDto.getProductId(),
                 productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis()- startTime));
@@ -34,12 +37,17 @@ public class ProductController {
     }
 
     @PostMapping(value="/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
 
-        return productService.saveProduct(productId, productName, productPrice, productStock);
+        ProductDto response = productService.saveProduct(productId, productName, productPrice, productStock);
+
+        LOGGER.info("[createProduct] Response >> productId : {}, productName : {}, productPrice : {}, productStock : {}", response.getProductId(),
+                response.getProductName(), response.getProductPrice(), response.getProductStock());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 }
--- src/main/java/studio/square/testproject/data/dto/ProductDto.java
+++ src/main/java/studio/square/testproject/data/dto/ProductDto.java
@@ -3,6 +3,10 @@ package studio.square.testproject.data.dto;
 import lombok.*;
 import studio.square.testproject.data.entity.ProductEntity;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,9 +14,20 @@ import studio.square.testproject.data.entity.ProductEntity;
 @Builder
 public class ProductDto {
 
+    @NotNull
     private String productId;
+
+    @NotNull
     private String productName;
+
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 3000000)
     private int productPrice;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 9999)
     private int productStock;
 
     public ProductEntity toEntity() {
