2c0a060f71ad7ea3d3d757a32e9bc6c0042adc11
squareyun <squareyun@gmail.com>
2022-05-09 23:14:29 +0900

    예외 처리 (Exception)

--- src/main/java/studio/square/testproject/exception/AroundHubExceptionHandler.java
+++ src/main/java/studio/square/testproject/exception/AroundHubExceptionHandler.java
@@ -1,39 +1,21 @@
-package studio.square.testproject.controller;
+package studio.square.testproject.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import studio.square.testproject.controller.HelloController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
-public class HelloController {
+@RestControllerAdvice
+public class AroundHubExceptionHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
-
-    //@RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @GetMapping("hello")
-    public String hello() {
-        return "Hello World!";
-    }
-
-    @PostMapping("log-test")
-    public void logTest() {
-        LOGGER.trace("Trace Log");
-        LOGGER.debug("Debug Log");
-        LOGGER.info("Info Log");
-        LOGGER.warn("Warn Log");
-        LOGGER.error("Error Log");
-    }
-
-    @PostMapping("/exception")
-    public void exceptionTest() throws Exception {
-        throw new Exception();
-    }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
@@ -41,7 +23,7 @@ public class HelloController {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
         LOGGER.info(e.getLocalizedMessage());
-        LOGGER.info("Controller 내 ExceptionHandler 호출");
+        LOGGER.info("Advice 내 ExceptionHandler 호출");
 
         Map<String, String> map = new HashMap<>();
         map.put("error type", httpStatus.getReasonPhrase());
